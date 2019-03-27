4d81457ab323c5db2445c6832af8fce6d12d69f9
gehsuyue1998 <1307378353@qq.com>
2019-03-27 16:21:36 +0800

    spa

--- 01-timer-button/main.js
+++ 01-timer-button/main.js
@@ -1,3 +1,17 @@
 $(function(){
-  console.log('hello world!');
+  var n=6;
+  var $btnArgee=$('input[type="button"]');
+  var timer=window.setInterval(funtion(){
+    n--;
+    if(n===0){
+      window.clearInterval(timer);
+      $btnArgee.removeAttr('disabled');
+      $btnargee.val('同意');
+    }else{
+      $btnArgee.val('同意('+n+'s)');
+    }
+  },1000);
+  $btnArgee.click(funtion(){
+    alert('SPA! 01-timer-button task!')
+  });
 });
