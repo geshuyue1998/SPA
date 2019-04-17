b89b66ce9c4cb9e2480f3953396cd2714e97a43a
gehsuyue1998 <1307378353@qq.com>
2019-04-17 16:49:26 +0800

    二进制函数曲线

--- 31-echart/main.js
+++ 31-echart/main.js
@@ -1,18 +1,35 @@
 $(function(){
-  var myChart = echarts.init(document.getElementById('main'));
-// 指定图表的配置项和数据
-     var option = {
-        title: {
-           text: 'ECharts 入门示例'
-        },
-       tooltip: {},                                       
-legend:{                                        
-  data:['销量'] },           
-xAxis: {
-            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]  },
-       yAxis: {},
-       series: [
-  name: '销量',                                   
-type: 'bar',
-         data: [5, 20, 36, 10, 10, 20]                 }]
-     myChart.setOption(option);                                                                     })                                                                      
+  var myChart = echarts.init($('.main')[0]);
+  var xData=[],
+      yData=[];
+  for(var p=0;p<=1;p+=0.1){
+    xData.push(p);
+    yData.push(h(p));
+  } 
+  function h(p){
+    if(p===0) {
+      return 0;
+    }
+    return -1*p*Math.log2(p)-(1-p)*Math.log2(1-p);
+  }
+  var option = {
+    title: {
+      text: '二进熵函数曲线'                
+    },
+    tooltip: {},
+    legend: {
+      data:['二进熵']            
+    },
+    xAxis: {
+      data: xData             
+    },
+    yAxis: {},
+    series: [{
+      name: '二进熵',
+      type: 'line',
+      smooth:true,
+      data: yData
+    }]  
+  };
+  myChart.setOption(option);
+});
