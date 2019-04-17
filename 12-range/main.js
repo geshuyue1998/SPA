a296ff4386852238ba6402706bf4d18ae07b16f8
gehsuyue1998 <1307378353@qq.com>
2019-04-17 15:28:09 +0800

    定时器按钮和滑杆

--- 12-range/main.js
+++ 12-range/main.js
@@ -1,3 +1,7 @@
 $(function(){
-  console.log('hello world!');
+  var $range=$('input[type="range"]');
+  var $txtAge=$('#age');
+  $range.change(function(){
+    $txtAge.html($range.val());
+  });
 });
